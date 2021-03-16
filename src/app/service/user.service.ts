ad9117ae23c13bc07768654b995240f79cc1cf8a
Zsofi <galambvarizsofia@gmail.com>
2021-03-16 22:21:29 +0100

    remove,update,delete

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -35,19 +35,27 @@ export class UserService {
    * Delete a user from the database.
    * The method is: this.http.delete
    */
-
+  remove(user: User): Observable<User> {
+    return this.http.delete<User>(`${this.endpoint}/${user.id}`);
+  }
 
 
   /**
    * Create a user in the database.
    * The method is: this.http.post
    */
-
+  create(user: User): Observable<User> {
+    return this.http.post<User>(this.endpoint, user);
+  }
 
 
   /**
    * Update a user in the database.
    * The method is: this.http.patch
    */
+  update(user: User): Observable<User> {
+    return this.http.patch<User>(`${this.endpoint}/${user.id}`, user);
+  }
+
 
 }
